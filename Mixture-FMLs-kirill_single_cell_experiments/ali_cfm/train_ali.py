efa836400c11ac49133564e6bcb6b89d906c2cce
okviman <okviman@kth.se>
2025-08-15 11:03:22 +0200

    Implemented RpGAN + factorized MMOT for the scRNA-seq experiments

---
+++ Mixture-FMLs-kirill_single_cell_experiments/ali_cfm/train_ali.py
@@ -0,0 +1,353 @@
+import torch
+from torch import nn
+
+import os
+import wandb
+import hydra
+import warnings
+from matplotlib import pyplot as plt
+from tqdm.auto import trange, tqdm
+
+from omegaconf import OmegaConf
+from sklearn.decomposition import PCA
+
+from torchdyn.core import NeuralODE
+from torchcfm.utils import torch_wrapper
+from torchcfm.conditional_flow_matching import OTPlanSampler
+
+from data import (
+    get_dataset,
+    denormalize, 
+    denormalize_gradfield
+)
+
+from utils import (
+    sample_gan_batch, 
+    sample_x0_x1, 
+    compute_emd,
+    fix_seed,
+    integrate_interpolant,
+    remove,
+    finish_results_table,
+)
+from interpolants import TrainableInterpolant, Discriminator, MLP
+
+
+def pretain_interpolant(interpolant, pretrain_optimizer_G, ot_sampler, 
+                        train_data, n_pretrain_epochs, gan_batch_size, 
+                        train_timesteps, ot='none', metric_prefix="", device='cpu'):
+    if n_pretrain_epochs == 0:
+        return
+    
+    for epoch in trange(n_pretrain_epochs, desc="Pretraining Interpolant", leave=False):
+        pretrain_optimizer_G.zero_grad()
+        batch = sample_gan_batch(train_data, gan_batch_size, 
+                                         ot_sampler=ot_sampler,
+                                         ot=ot, times=train_timesteps)
+        x0, x1, xt, t = (x.to(device) for x in batch)
+        xt_fake = interpolant(x0, x1, t)
+
+        loss = (xt_fake - xt).pow(2).mean()
+        loss.backward()
+
+        pretrain_optimizer_G.step()
+        wandb.log({
+            f"{metric_prefix}/pretrain_loss": loss.item(), 
+            f"{metric_prefix}_step": epoch}
+        )
+
+
+def train_interpolant_with_gan(
+    interpolant, discriminator, ot_sampler, data,
+    gan_optimizer_G, gan_optimizer_D, n_epochs,
+    gan_batch_size, correct_coeff, train_timesteps, seed, min_max,
+    ot='none', plot_freaquency=5000, metric_prefix="", device='cpu', gan_loss='vanilla'
+):
+    t_max = max(train_timesteps)
+    curr_epoch = 0
+    for epoch in trange(curr_epoch, n_epochs, 
+                        desc="Training GAN Interpolant", leave=False):
+        if epoch > 40_000:
+            gan_optimizer_G.param_groups[0]['lr'] = 1e-5
+            gan_optimizer_D.param_groups[0]['lr'] = 5e-5
+        elif epoch > 100_000:
+            gan_optimizer_G.param_groups[0]['lr'] = 1e-6
+            gan_optimizer_D.param_groups[0]['lr'] = 5e-6
+        
+        curr_epoch += 1
+        if epoch % 5_000 == 0:
+            with torch.no_grad():
+                for time in range(1, 4):
+                    test_batch = sample_gan_batch(
+                        data, 2300, ot_sampler=ot_sampler, 
+                        time=time, ot='border')
+                    x0_test, x1_test, xt_test, t_test = (
+                        x.to(device) for x in test_batch
+                    )
+                    
+                    xt_fake_test = interpolant(x0_test, x1_test, t_test)
+                    emd_t = compute_emd(
+                            denormalize(xt_test, min_max), 
+                            denormalize(xt_fake_test, min_max)
+                        )
+                    wandb.log({
+                        f"{metric_prefix}/emd_t={time}": emd_t.item(), 
+                        f"{metric_prefix}_step": epoch
+                    })
+                    
+
+        batch = sample_gan_batch(data, gan_batch_size, 
+                                 ot_sampler=ot_sampler, ot=ot, 
+                                 times=train_timesteps)
+        x0, x1, xt, t = (x.to(device) for x in batch)
+        xt_fake = interpolant(x0, x1, t)
+
+        real_inputs = torch.cat([xt, t], dim=-1)
+        fake_inputs = torch.cat([xt_fake.detach(), t], dim=-1)
+        
+        real_proba = discriminator(real_inputs)
+        fake_proba = discriminator(fake_inputs)
+
+        # Train discriminator
+        gan_optimizer_D.zero_grad()
+        if gan_loss == "RpGAN":
+            # Using the relativistic pairing GAN loss
+            d_loss = torch.nn.functional.softplus((fake_proba - real_proba)).mean()
+
+            d_loss.backward()
+            gan_optimizer_D.step()
+
+            # Train generator
+            gan_optimizer_G.zero_grad()
+            real_inputs = torch.cat([xt, t], dim=-1)
+            fake_inputs = torch.cat([xt_fake, t], dim=-1)
+
+            real_proba = discriminator(real_inputs)
+            fake_proba = discriminator(fake_inputs)
+            g_loss_ = torch.nn.functional.softplus((real_proba - fake_proba)).mean()
+        else:
+            d_real_loss = nn.functional.softplus(-real_proba).mean()
+            d_fake_loss = nn.functional.softplus(fake_proba).mean()
+            d_loss = d_real_loss + d_fake_loss
+
+            d_loss.backward()
+            gan_optimizer_D.step()
+
+            # Train generator
+            gan_optimizer_G.zero_grad()
+            fake_inputs = torch.cat([xt_fake, t], dim=-1)
+            fake_proba = discriminator(fake_inputs)
+
+            g_loss_ = nn.functional.softplus(-fake_proba).mean()
+        reg_weight_loss = interpolant.get_reg_term(x0, x1, t, xt_fake)
+        g_loss = g_loss_ + correct_coeff * reg_weight_loss
+        
+        g_loss.backward()
+        gan_optimizer_G.step()
+
+        wandb.log({
+            f"{metric_prefix}/d_loss": d_loss.item(),
+            f"{metric_prefix}/g_loss": g_loss.item(),
+            f"{metric_prefix}/reg_weight_loss": reg_weight_loss.item(),
+            f"{metric_prefix}/fake_proba": fake_proba.mean().item(),
+            f"{metric_prefix}/real_proba": real_proba.mean().item(),
+            f"{metric_prefix}_step": epoch
+        })
+
+        if epoch % plot_freaquency == 0:
+            with torch.no_grad():
+                batch = sample_gan_batch(data, 256, ot_sampler=ot_sampler, 
+                                         ot='full', times=train_timesteps)
+                x0, x1, xt, t = (x.to(device) for x in batch)
+                xt_fake = interpolant(x0, x1, t)
+
+                pca = PCA(n_components=2, random_state=seed)
+                
+                xt_pca = pca.fit_transform(xt.cpu())
+                xt_fake_pca = pca.transform(xt_fake.cpu())
+                fig = plt.figure()
+                
+                plt.scatter(*xt_fake_pca.T, c='red', alpha=0.5, label="Fake")
+                plt.scatter(*xt_pca.T, c='blue', alpha=0.5, label="Real")
+                plt.legend()
+                plt.title(f"PCA of `True` and `Fake` samples for t={int(t[0] * t_max)}")
+
+                wandb.log({
+                    f"{metric_prefix}/scatter_image": wandb.Image(fig), 
+                    f"{metric_prefix}_step": epoch
+                })
+                plt.close(fig) 
+
+
+def train_ot_cfm(ot_cfm_model, ot_cfm_optimizer, interpolant, 
+                 ot_sampler, train_data, batch_size, min_max,
+                 n_ot_cfm_epochs, metric_prefix="", ot='border', device='cpu'):
+    for step in trange(n_ot_cfm_epochs, 
+                       desc="Training OT CFM Interpolant", leave=False):
+        ot_cfm_optimizer.zero_grad()
+
+        x0, x1 = sample_x0_x1(train_data, batch_size, device=device)
+        
+        if ot == 'border' or ot == 'full':
+            x0, x1 = ot_sampler.sample_plan(x0, x1)
+        
+        t = torch.rand(x0.shape[0], 1, device=device)
+        
+        xt = interpolant(x0, x1, t).detach()
+        ut = interpolant.dI_dt(x0, x1, t).detach()
+
+        ut = denormalize_gradfield(ut, min_max).float()
+        xt = denormalize(xt, min_max).float()
+        
+        vt = ot_cfm_model(torch.cat([xt, t], dim=-1))
+        
+        loss = torch.mean((vt - ut) ** 2)
+        loss.backward()
+        ot_cfm_optimizer.step()
+        
+        wandb.log({
+            f"{metric_prefix}/cfm_loss": loss.item(),
+            f"{metric_prefix}_cfm_step": step
+        })
+
+
+@hydra.main(config_path="./configs", config_name="ali")
+def train_ali(cfg):
+    os.environ["HYDRA_FULL_ERROR"] = '1'
+    seed_list = cfg.seed_list
+    warnings.filterwarnings("ignore")
+    ot_sampler = OTPlanSampler('exact', reg=0.1)
+
+    data, min_max = get_dataset(cfg.dataset, cfg.n_data_dims, 
+                                cfg.normalize_dataset, cfg.whiten)
+    timesteps_list = [t for t in range(len(data))]
+    # This code assumes that timesteps are in [0, ..., T_max]
+    num_int_steps = cfg.num_int_steps_per_timestep * max(timesteps_list)
+    
+    int_results, cfm_results = {}, {}
+
+    run = wandb.init(
+        name=f"{cfg.wandb_name}-{cfg.dataset}-{cfg.n_data_dims}D",
+        mode=cfg.wandb_mode,
+        project="ali-cfm",
+        config=OmegaConf.to_object(cfg)
+    )
+
+    os.makedirs(f"{run.dir}/checkpoints", exist_ok=True)
+    
+    for seed in tqdm(seed_list, desc="Seeds"):
+        fix_seed(seed)
+        int_results[f"seed={seed}"] = []
+        cfm_results[f"seed={seed}"] = []
+
+        for removed_t in tqdm(timesteps_list[1: -1], desc="Timesteps", leave=False):
+            curr_timesteps = remove(timesteps_list, removed_t)
+            metric_prefix = f"t={removed_t}_{seed=}"
+            
+            # Configure neural networks
+            interpolant = TrainableInterpolant(cfg.dim, cfg.net_hidden, 0.01, True).to(cfg.device)
+            pretrain_optimizer_G = torch.optim.Adam(interpolant.parameters(), lr=1e-3)
+
+            discriminator = Discriminator(
+                cfg.dim + 1, cfg.net_hidden, apply_sigmoid=False
+            ).to(cfg.device)
+            gan_optimizer_G = torch.optim.Adam(interpolant.parameters(), lr=1e-3)
+            gan_optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=1e-4)
+
+            ot_cfm_model = MLP(dim=cfg.dim, time_varying=True, w=cfg.net_hidden).to(cfg.device)
+            ot_cfm_optimizer = torch.optim.Adam(ot_cfm_model.parameters(), 1e-3)
+            
+            # Pretrain interpolant
+            pretain_metric_prefix = f"{metric_prefix}_pretrain"
+            wandb.define_metric(f"{pretain_metric_prefix}/*",
+                                step_metric=f"{pretain_metric_prefix}_step")
+            pretain_interpolant(
+                interpolant, pretrain_optimizer_G, ot_sampler, data,
+                cfg.n_pretrain_epochs, cfg.batch_size, curr_timesteps, 
+                ot=cfg.pretain_ot, metric_prefix=pretain_metric_prefix,
+                device=cfg.device
+            )
+
+            # Train interpolant with GAN
+            interpolant_metric_prefix = f"{metric_prefix}_interpolant"
+            wandb.define_metric(f"{interpolant_metric_prefix}/*",
+                                step_metric=f"{interpolant_metric_prefix}_step")
+
+            train_interpolant_with_gan(
+                interpolant, discriminator, ot_sampler,
+                data,
+                gan_optimizer_G, gan_optimizer_D, 
+                cfg.n_epochs, cfg.batch_size, cfg.correct_coeff,
+                curr_timesteps, seed, min_max, ot=cfg.interpolant_ot, 
+                metric_prefix=interpolant_metric_prefix,
+                device=cfg.device, gan_loss=cfg.gan_loss
+            )
+            
+            # Compute metrics for GAN interpolant
+            x0 = data[0].to(cfg.device)
+            x1 = data[-1].to(cfg.device)
+            x0, x1 = ot_sampler.sample_plan(x0, x1)
+
+            int_traj = integrate_interpolant(
+                x0, x1, num_int_steps, interpolant,
+            )
+
+            int_emd = compute_emd(
+                denormalize(data[removed_t], min_max), 
+                denormalize(int_traj[100 * removed_t], min_max)
+            )
+            int_results[f"seed={seed}"].append(int_emd.item())
+            
+            # Train OT-CFM using GAN interpolant
+            cfm_metric_prefix = f"{metric_prefix}_cfm"
+            wandb.define_metric(f"{cfm_metric_prefix}/*",
+                                step_metric=f"{cfm_metric_prefix}_step")
+            train_ot_cfm(
+                ot_cfm_model, ot_cfm_optimizer, interpolant, ot_sampler,
+                data, cfg.batch_size, min_max, cfg.n_ot_cfm_epochs,
+                metric_prefix=cfm_metric_prefix, ot=cfg.cfm_ot,
+                device=cfg.device
+            )
+
+            # Compute metrics for OT-CFM
+            node = NeuralODE(torch_wrapper(ot_cfm_model),
+                            solver="dopri5", sensitivity="adjoint")
+
+            with torch.no_grad():
+                ot_cfm_traj = node.trajectory(
+                    denormalize(data[0], min_max).to(cfg.device),
+                    t_span=torch.linspace(0, 1, num_int_steps + 1),
+                )
+
+            cfm_emd = compute_emd(
+                denormalize(data[removed_t], min_max).to(cfg.device), 
+                ot_cfm_traj[100 * removed_t].float().to(cfg.device),
+            )
+            cfm_results[f"seed={seed}"].append(cfm_emd.item())
+
+            # Save artifacts for given seed and t
+            checkpoint = {
+                "interpolant": interpolant.state_dict(),
+                "discriminator": discriminator.state_dict(),
+                "ot_cfm_model": ot_cfm_model.state_dict(),
+            }
+            save_path = os.path.join(
+                run.dir, "checkpoints", f"{metric_prefix}_ali_cfm.pth"
+            )
+            torch.save(checkpoint, save_path)
+
+    int_results = wandb.Table(
+        dataframe=finish_results_table(int_results, timesteps_list[1: -1])
+    )
+    wandb.log({"interpolant_results": int_results})
+
+    cfm_results = wandb.Table(
+        dataframe=finish_results_table(cfm_results, timesteps_list[1: -1])
+    )
+    wandb.log({"cfm_results": cfm_results})
+    
+    wandb.finish()
+
+    
+if __name__ == "__main__":
+    train_ali()
---
+++ Mixture-FMLs-kirill_single_cell_experiments/ali_cfm/utils.py
@@ -0,0 +1,223 @@
+import random
+
+import math
+import torch
+import ot as pot
+import numpy as np
+import pandas as pd
+
+from pathlib import Path
+
+from tqdm import trange
+
+
+def fix_seed(seed: int = 42):
+    random.seed(seed)               
+    np.random.seed(seed)            
+    torch.manual_seed(seed)         
+    torch.cuda.manual_seed(seed)    
+    torch.cuda.manual_seed_all(seed)
+
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+def get_batch(FM, X, batch_size, timesteps, return_noise=False, device='cpu'):
+    """Construct a batch with point sfrom each timepoint pair"""
+    ts = []
+    xts = []
+    uts = []
+    noises = []
+    for t_curr in range(len(timesteps) - 1):
+        t_start = timesteps[t_curr]
+        t_end = timesteps[t_curr + 1]
+
+        idx = np.random.randint(X[t_start].shape[0], size=batch_size)
+        x0 = X[t_start][idx].float().to(device)
+        
+        idx = np.random.randint(X[t_end].shape[0], size=batch_size)
+        x1 = X[t_end][idx].float().to(device)
+        
+        if return_noise:
+            t, xt, ut, eps = FM.sample_location_and_conditional_flow(
+                x0, x1, return_noise=return_noise, ts=0, te=t_end - t_start
+            )
+            noises.append(eps)
+        else:
+            t, xt, ut = FM.sample_location_and_conditional_flow(
+                x0, x1, return_noise=return_noise, ts=0, te=t_end - t_start
+            )
+
+        ts.append(t + t_start)
+        xts.append(xt)
+        uts.append(ut)
+
+    t = torch.cat(ts)
+    xt = torch.cat(xts)
+    ut = torch.cat(uts)
+
+    if return_noise:
+        noises = torch.cat(noises)
+        return t, xt, ut, noises
+    return t, xt, ut
+
+
+def get_batch_for_cubic(FM, X, batch_size, timesteps, device='cpu'):
+    batch = [x[:batch_size] for t, x in enumerate(X) if t in timesteps]
+    batch = torch.from_numpy(np.stack(batch)).float().to(device)
+    
+    timesteps = torch.cat(
+        [torch.ones(1, batch_size) * t for t in timesteps], 
+        0).to(device)
+    
+
+    t, xt, ut, *_ = FM.sample_location_and_conditional_flow(
+        batch.permute(1, 0, 2), timesteps.permute(1, 0)
+    )
+
+    xt = xt.squeeze(1)
+    ut = ut.squeeze(1)
+    
+    return t, xt, ut
+
+
+def sample_x0_x1(X, batch_size, device='cpu'):
+    x0 = X[0][np.random.randint(X[0].shape[0], size=batch_size)].to(device)
+    x1 = X[-1][np.random.randint(X[-1].shape[0], size=batch_size)].to(device)
+    return x0, x1
+
+    
+def init_weights(m):
+    if isinstance(m, torch.nn.Linear):
+        torch.nn.init.xavier_normal_(m.weight, gain=3.6)
+        m.bias.data.fill_(0.00)
+
+
+def compute_window_avg(array, window_size):
+    return np.convolve(array, np.ones(window_size), mode='valid') / window_size
+
+
+@torch.no_grad()
+def compute_emd(p1, p2, p=1, device='cpu'):
+    a_weights = torch.ones((p1.shape[0],), device=device) / p1.shape[0]
+    b_weights = torch.ones((p2.shape[0],), device=device) / p2.shape[0]
+
+    M = pot.dist(p1, p2, p=p)
+    dist = pot.emd2(a_weights, b_weights, M, numItermax=1e7)
+    
+    if p == 2:
+        return math.sqrt(dist)
+    return dist
+
+
+def sample_x_batch(X, batch_size):
+    return X[np.random.randint(0, X.shape[0], size=batch_size)]
+
+
+def sample_deterministic_ot_plan(x0, x1, ot_sampler):
+    pi = ot_sampler.get_map(x0, x1)
+    pi = pi / pi.sum()
+    return x0, x1[pi.argmax(axis=1)]
+
+
+def sample_gan_batch(X, batch_size, ot_sampler, 
+                     time=None, ot='none', times=(1, 2, 3)):
+    time = time or random.choice(times)
+    x0 = sample_x_batch(X[0], batch_size)
+    x1 = sample_x_batch(X[-1], batch_size)
+    xt = sample_x_batch(X[time], batch_size)
+    t = torch.ones(batch_size, 1) * time / 4
+
+    if ot == 'full':
+        x0, xt = sample_deterministic_ot_plan(x0, xt, ot_sampler)
+        xt, x1 = sample_deterministic_ot_plan(xt, x1, ot_sampler)
+    elif ot == 'border':
+        x0, x1 = ot_sampler.sample_plan(x0, x1)
+    elif ot == 'mmot':
+        x0, x1 = mmot_couple_marginals(x0, x1, xt, ot_sampler)
+    elif ot == 'none':
+        pass
+    else:
+        raise ValueError(f"Unknown OT type: {ot}")
+
+    return x0, x1, xt, t
+
+
+def integrate_interpolant(x0, x1, n_steps, interpolant):
+    device = x0.device
+    dt = 1 / n_steps
+    t = torch.zeros(x0.shape[0], 1, device=device)
+
+    integrated_trajectories = [x0.detach().cpu()]
+    for _ in trange(n_steps, leave=False):
+        xtm1 = integrated_trajectories[-1].to(device)
+        xtm1 = xtm1 + interpolant.dI_dt(x0, x1, t) * dt
+        integrated_trajectories.append(xtm1.cpu().detach())
+        t += dt
+
+    return torch.stack(integrated_trajectories)
+
+
+def remove(arr, val):
+    return [x for x in arr if x != val]
+
+
+def finish_results_table(table, timesteps):
+    table = pd.DataFrame(table)
+    table["mean"] = table.mean(axis=1)
+    table["std"] = table.std(axis=1)
+    table['timesteps'] = [f"{t=}" for t in timesteps]
+    table = table.round(4)
+    return table
+
+def get_run_dir(run_id: str, search_dir: str | Path = './wandb'):
+    found_dirs = [*Path(search_dir).glob(f"*{run_id}*")]
+    if not found_dirs:
+        raise FileNotFoundError(f"No directories found for run_id: {run_id}")
+    return found_dirs[0] / 'files'
+
+def mmot_couple_marginals(X0, X1, Xt, otplan):
+    """
+        Samples bs triplets (x0, xt, x1) from the factorized joint
+           π*(x0, xt, x1) ∝ π1*(x0, xt) · π2*(xt, x1) / μt(xt)
+        assuming μt is uniform.
+
+        Args:
+            minibatch samples from the three marginals
+            X0: (bs, d)
+            X1: (bs, d)
+            Xt: (bs, d)
+            bs: number of samples to draw (with replacement)
+
+        Returns:
+            aligned: Tensor of shape (bs, 3, d)
+        """
+    # X0, Xt, X1 = X
+    bs, d = X0.shape
+
+    device = X0.device
+    aligned = torch.zeros((bs, 3, d), device=device)
+
+    # 1) compute the two pairwise plans as numpy arrays
+    pi1_np = otplan.get_map(X0, Xt)  # shape (n0, nt)
+    pi2_np = otplan.get_map(Xt, X1)  # shape (nt, n1)
+
+
+    # 2) convert to torch and move to device
+    pi1 = torch.from_numpy(pi1_np).to(device)  # (n0, nt)
+    pi2 = torch.from_numpy(pi2_np).to(device)  # (nt, n1)
+
+    idx_t = torch.tensor(np.arange(0, bs), dtype=torch.int, device=device)
+
+    # 4) sample x0 | xt  using columns of pi1
+    probs0 = pi1[:, idx_t].t()
+    probs0 = probs0 / probs0.sum(dim=1, keepdim=True)
+    idx_0 = torch.multinomial(probs0, num_samples=1, replacement=False).squeeze(1)
+
+    # 5) sample x1 | xt using rows of pi2
+    probs2 = pi2[idx_t, :]  # (bs, n1)
+    probs2 = probs2 / probs2.sum(dim=1, keepdim=True)
+    idx_1 = torch.multinomial(probs2, num_samples=1, replacement=False).squeeze(1)
+
+    # 6) return the coupled points
+    return X0[idx_0], X1[idx_1]
